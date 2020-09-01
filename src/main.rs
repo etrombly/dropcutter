d956d34dccfda27a83e29c8c217b389c7ad260f1
Eric Trombly <etrombly@yahoo.com>
2020-09-01 10:47:54 -0500

    make tool paths alternate direction

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use float_cmp::approx_eq;
 use printer_geo::{compute::*, geo::*, util::*};
 use rayon::prelude::*;
 use std::{
@@ -143,18 +144,54 @@ fn main() {
 
         println!("tool bounds: {:?}", tool.bbox);
     }
-    let tri_vk = to_tri_vk(&triangles);
     let bounds = get_bounds(&triangles);
+    /*
+    println!("{:?}", bounds);
+    let triangles: Vec<_> = triangles
+        .par_iter()
+        .map(|x| {
+            Triangle3d::new(
+                (
+                    x.p1.x - bounds.p1.x,
+                    x.p1.y - bounds.p1.y,
+                    x.p1.z,
+                ),
+                (
+                    x.p2.x - bounds.p1.x,
+                    x.p2.y - bounds.p1.y,
+                    x.p2.z - bounds.p1.z,
+                ),
+                (
+                    x.p3.x - bounds.p1.x,
+                    x.p3.y - bounds.p1.y,
+                    x.p3.z - bounds.p1.z,
+                ),
+            )
+        })
+        .collect();
+    let bounds = get_bounds(&triangles);
+    println!("{:?}", bounds);
+    */
+    let tri_vk = to_tri_vk(&triangles);
+
     let max_x = (bounds.p2.x * 10.) as i32;
     let min_x = (bounds.p1.x * 10.) as i32;
     let max_y = (bounds.p2.y * 10.) as i32;
     let min_y = (bounds.p1.y * 10.) as i32;
+    let mut rev = false;
     let tests: Vec<PointVk> = (min_x..max_x)
         .step_by((radius * 10.) as usize)
         .flat_map(|x| {
-            (min_y..max_y)
+            rev = !rev;
+            if rev {
+            (min_y..max_y).rev()
                 .map(move |y| PointVk::new(x as f32 / 10.0, y as f32 / 10.0, bounds.p2.z + 1.))
                 .collect::<Vec<_>>()
+            } else {
+                (min_y..max_y)
+                .map(move |y| PointVk::new(x as f32 / 10.0, y as f32 / 10.0, bounds.p2.z + 1.))
+                .collect::<Vec<_>>()
+            }
         })
         .collect();
     println!(
@@ -179,15 +216,51 @@ fn main() {
     file.write_all(output.as_bytes()).unwrap();
 
     let mut file = File::create("out.gcode").unwrap();
-    let output = result
-        .iter()
-        .map(|x| {
-            format!(
-                "G0 X{:.3} Y{:.3} Z{:.3}\n",
-                x.position[0], x.position[1], x.position[2]
-            )
-        })
-        .collect::<Vec<String>>()
-        .join("");
+    let mut last = result[0];
+    let mut last_diff = 0;
+    let mut output = format!(
+        "G0 X{:.3} Y{:.3} Z{:.3}\n",
+        last.position[0], last.position[1], last.position[2]
+    )
+    .to_string();
+    for line in result {
+        let diff = difference(&last, &line);
+        // if check_diff(diff, last_diff) {
+        // output.push_str(&format!(
+        //    "G0 X{:.3} Y{:.3} Z{:.3}\n",
+        //    last.position[0], last.position[1], last.position[2]
+        //));
+        output.push_str(&format!(
+            "G0 X{:.3} Y{:.3} Z{:.3}\n",
+            line.position[0], line.position[1], line.position[2]
+        ));
+        //}
+        last_diff = diff;
+        last = line;
+    }
     file.write_all(output.as_bytes()).unwrap();
+}
+
+pub fn difference(left: &PointVk, right: &PointVk) -> u8 {
+    let mut result = 0;
+    if !approx_eq!(f32, left.position[0], right.position[0], ulps = 3) {
+        result += 1
+    }
+    if !approx_eq!(f32, left.position[1], right.position[1], ulps = 3) {
+        result += 2
+    }
+    if !approx_eq!(f32, left.position[2], right.position[2], ulps = 3) {
+        result += 4
+    }
+    result
+}
+
+pub fn check_diff(current: u8, last: u8) -> bool {
+    if current != last {
+        true
+    } else if [1, 2, 4].contains(&current) {
+        false
+    } else {
+        true
+    }
 }
