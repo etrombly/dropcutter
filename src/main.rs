19e55ae17b2415c1adcd4a06e1df444ff748b1a0
Eric Trombly <etrombly@yahoo.com>
2020-09-23 10:19:52 -0500

    more work on path generation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use float_cmp::approx_eq;
 use indicatif::{MultiProgress, ProgressBar, ProgressDrawTarget, ProgressStyle, TickTimeLimit};
-use printer_geo::{bfs::*, compute::*, config::*, geo::*, stl::*};
+use printer_geo::{bfs::*, compute::*, config::*, stl::*};
 use rayon::prelude::*;
 use std::{
     fs::File,
@@ -45,9 +45,7 @@ fn main() -> Result<()> {
     // parse input args, may remove this once I build the GUI
     let opt = Opt::from_args();
 
-    let mp = Arc::new(MultiProgress::with_draw_target(
-        ProgressDrawTarget::stderr_nohz(),
-    ));
+    let mp = Arc::new(MultiProgress::with_draw_target(ProgressDrawTarget::stderr_nohz()));
     let partition_bar = mp.add(ProgressBar::new(0));
     partition_bar.set_style(ProgressStyle::default_bar().template("[1/5] Filtering mesh"));
     partition_bar.tick();
@@ -112,9 +110,7 @@ fn main() -> Result<()> {
 
     let clock = std::time::Instant::now();
     let partition = partition_tris(&triangles, &columns, &vk).unwrap();
-    partition_bar.set_style(
-        ProgressStyle::default_bar().template("[1/5] Filtering mesh elapsed: {elapsed}"),
-    );
+    partition_bar.set_style(ProgressStyle::default_bar().template("[1/5] Filtering mesh elapsed: {elapsed}"));
     partition_bar.finish();
     total_bar.tick();
     if opt.debug {
@@ -123,9 +119,7 @@ fn main() -> Result<()> {
 
     let clock = std::time::Instant::now();
     height_bar.set_length(grid.len() as u64 / 10);
-    height_bar.set_style(
-        ProgressStyle::default_bar().template("[2/5] Computing height map {bar:40.cyan/blue}"),
-    );
+    height_bar.set_style(ProgressStyle::default_bar().template("[2/5] Computing height map {bar:40.cyan/blue}"));
     height_bar.reset_elapsed();
     let heightmap: Vec<Vec<_>> = match opt.heightmap {
         Some(file) => {
@@ -143,9 +137,7 @@ fn main() -> Result<()> {
         _ => generate_heightmap(grid, partition, &height_bar, &total_bar, &vk),
     };
 
-    height_bar.set_style(
-        ProgressStyle::default_bar().template("[2/5] Computing height map elapsed: {elapsed}"),
-    );
+    height_bar.set_style(ProgressStyle::default_bar().template("[2/5] Computing height map elapsed: {elapsed}"));
     height_bar.finish();
     if opt.debug {
         println!("drop time {:?}", clock.elapsed());
@@ -160,16 +152,14 @@ fn main() -> Result<()> {
     }
 
     let clock = std::time::Instant::now();
-    let columns = heightmap.len();
+    let segments = heightmap.len();
     let rows = heightmap[0].len();
     // process height map with selected tool to find heights
-    let count = columns / (radius * stepover * scale).ceil() as usize;
+    let count = segments / (radius * stepover * scale).ceil() as usize;
     tool_bar.set_length(count as u64);
     tool_bar.reset_elapsed();
-    tool_bar.set_style(
-        ProgressStyle::default_bar().template("[3/5] Processing tool path {bar:40.cyan/blue}"),
-    );
-    let processed: Vec<Vec<_>> = ((radius * scale) as usize..columns)
+    tool_bar.set_style(ProgressStyle::default_bar().template("[3/5] Processing tool path {bar:40.cyan/blue}"));
+    let processed: Vec<Vec<_>> = ((radius * scale) as usize..segments)
         .into_par_iter()
         // space each column based on radius and stepover
         .step_by((radius * stepover * scale).ceil() as usize)
@@ -194,7 +184,7 @@ fn main() -> Result<()> {
                             // for each point in the tool adjust it's location to the height map and calculate the intersection
                             let x_offset = (x as f32 + (tpoint.pos.x * scale)).round() as i32;
                             let y_offset = (y as f32 + (tpoint.pos.y * scale)).round() as i32;
-                            if x_offset < columns as i32
+                            if x_offset < segments as i32
                                 && x_offset >= 0
                                 && y_offset < rows as i32
                                 && y_offset >= 0
@@ -212,9 +202,7 @@ fn main() -> Result<()> {
                 .collect()
         })
         .collect();
-    tool_bar.set_style(
-        ProgressStyle::default_bar().template("[3/5] Processing tool path elapsed: {elapsed}"),
-    );
+    tool_bar.set_style(ProgressStyle::default_bar().template("[3/5] Processing tool path elapsed: {elapsed}"));
     tool_bar.finish();
     if opt.debug {
         println!("tool time {:?}", clock.elapsed());
@@ -227,9 +215,9 @@ fn main() -> Result<()> {
         let output = heightmap
             .iter()
             .flat_map(|column| {
-                column.iter().map(|point| {
-                    format!("{:.3} {:.3} {:.3}\n", point.pos.x, point.pos.y, point.pos.z)
-                })
+                column
+                    .iter()
+                    .map(|point| format!("{:.3} {:.3} {:.3}\n", point.pos.x, point.pos.y, point.pos.z))
             })
             .collect::<Vec<String>>()
             .join("");
@@ -245,9 +233,7 @@ fn main() -> Result<()> {
     let steps = ((bounds.p2.pos.z - bounds.p1.pos.z) / stepdown) as u64;
     layer_bar.set_length(steps);
     layer_bar.reset_elapsed();
-    layer_bar.set_style(
-        ProgressStyle::default_bar().template("[4/5] Processing layers {bar:40.cyan/blue}"),
-    );
+    layer_bar.set_style(ProgressStyle::default_bar().template("[4/5] Processing layers {bar:40.cyan/blue}"));
     let points: Vec<Vec<Vec<_>>> = (1..steps + 1)
         .map(|step| {
             layer_bar.inc(1);
@@ -265,9 +251,7 @@ fn main() -> Result<()> {
                 .collect()
         })
         .collect();
-    layer_bar.set_style(
-        ProgressStyle::default_bar().template("[4/5] Processing layers elapsed: {elapsed}"),
-    );
+    layer_bar.set_style(ProgressStyle::default_bar().template("[4/5] Processing layers elapsed: {elapsed}"));
     layer_bar.finish();
     if opt.debug {
         println!("multi-pass processing {:?}", clock.elapsed());
@@ -276,98 +260,60 @@ fn main() -> Result<()> {
     let clock = std::time::Instant::now();
     gcode_bar.set_length(points.len() as u64);
     gcode_bar.reset_elapsed();
-    gcode_bar.set_style(
-        ProgressStyle::default_bar().template("[5/5] Processing Gcode {bar:40.cyan/blue}"),
-    );
+    gcode_bar.set_style(ProgressStyle::default_bar().template("[5/5] Processing Gcode {bar:40.cyan/blue}"));
     let mut file = File::create(opt.output)?;
-    let mut last = processed[0][0];
     // start by moving to max Z
-    // TODO: add a safe travel height
     // TODO: add actual feedrate
     let mut output = format!("G0 Z{:.2} F300\n", 0.);
+    let mut last = Point3d::new(0., 0., 0.);
 
     for (layer_index, layer) in points.iter().enumerate() {
         gcode_bar.inc(1);
         total_bar.tick();
         let mut islands = get_islands(&layer, bounds.p2.pos.z - (stepdown * (layer_index as f32)));
-        for (num, island) in islands.iter_mut().enumerate() {
-            output.push_str(&format!(
-                "G0 Z{:.2}\n",
-                0.
-            ));
+        // TODO: sort islands by distance from each other
+        for island in islands.iter_mut() {
             island.sort();
-            //////// DEBUG
-            let mut file = File::create(format!("island{}.xyz", num))?;
-            let contents = island
-                .iter()
-                .map(|x| format!("{:.3} {:.3} {:.3}\n", x.pos.x, x.pos.y, x.pos.z))
-                .collect::<Vec<String>>()
-                .join("");
-            file.write_all(contents.as_bytes())?;
-            //////
 
-            let mut columns = partition_columns(&island);
-            let mut rev = false;
+            let mut segments = partition_segments(&island, opt.diameter);
+            let mut current_segment = get_next_segment(&mut segments, &last);
 
-            while let Some(mut column) = columns.pop() {
-                if rev {
-                    column.reverse();
-                }
-                rev = !rev;
-                //output.push_str(&format!(
-                //    "G1 X{:.2} Y{:.2}\n",
-                //    column[0].pos.x, column[0].pos.y
-                //));
-                let mut last: Option<Point3d> = None;
-                while let Some(point) = column.pop() {
-                    if last.is_none() {
-                        output.push_str(&format!(
-                            "G1 X{:.2} Y{:.2}\n",
-                            point.pos.x, point.pos.y
-                        ));
-                    } else if (point.pos.y - last.unwrap().pos.y).abs() > opt.resolution * 2. && column.len() > 0 {
-                        output.push_str("; skipping\n");
-                        output.push_str(&format!("G0 Z{:.2}\n", 0.));
-                        columns.insert(0, column);
-                        rev = !rev;
-                        break;
-                    }
+            while segments.len() > 0 {
+                let mut segment = segments.remove(current_segment);
+                // TODO: instead of retracting to safe height build a move to travel closer to
+                // model if the segments aren't adjacent retract and travel to
+                // next segment
+                if distance(&segment[segment.len() - 1].pos.xy(), &last.pos.xy()) > opt.diameter * 2. {
                     output.push_str(&format!(
-                        "G1 X{:.2} Y{:.2} Z{:.2}\n",
-                        point.pos.x, point.pos.y, point.pos.z
+                        "G0 Z0\nG0 X{:.2} Y{:.2}\n",
+                        segment[segment.len() - 1].pos.x,
+                        segment[segment.len() - 1].pos.y
                     ));
-                    last = Some(point);
                 }
+
+                // don't write a move if only Y changed
+                while let Some(point) = segment.pop() {
+                    if !approx_eq!(f32, last.pos.x, point.pos.x, ulps = 3)
+                        || !approx_eq!(f32, last.pos.z, point.pos.z, ulps = 3)
+                    {
+                        output.push_str(&format!(
+                            "G1 X{:.2} Y{:.2} Z{:.2}\n",
+                            point.pos.x, point.pos.y, point.pos.z
+                        ));
+                    }
+
+                    last = point;
+                }
+                output.push_str(&format!(
+                    "G1 X{:.2} Y{:.2} Z{:.2}\n",
+                    last.pos.x, last.pos.y, last.pos.z
+                ));
+                current_segment = get_next_segment(&mut segments, &last);
             }
             output.push_str(&format!("G0 Z{:.2}\n", 0.));
         }
-        /*
-        for row in layer {
-            output.push_str(&format!(
-                "G0 X{:.2} Y{:.2}\nG0 Z{:.2}\n",
-                row[0].pos.x, row[0].pos.y, row[0].pos.z
-            ));
-            for point in row {
-                //if !approx_eq!(f32, last.pos.x, point.pos.x, ulps = 3)
-                //    || !approx_eq!(f32, last.pos.z, point.pos.z, ulps = 3)
-                //{
-                    output.push_str(&format!(
-                        "G1 X{:.2} Y{:.2} Z{:.2}\n",
-                        point.pos.x, point.pos.y, point.pos.z
-                    ));
-                //}
-                last = point;
-            }
-            output.push_str(&format!(
-                "G1 X{:?} Y{:?} Z{:?}\nG0 Z{:.2}\n",
-                last.pos.x, last.pos.y, last.pos.z, bounds.p2.pos.z
-            ));
-        }
-        */
     }
-    gcode_bar.set_style(
-        ProgressStyle::default_bar().template("[5/5] Processing Gcode elapsed: {elapsed}"),
-    );
+    gcode_bar.set_style(ProgressStyle::default_bar().template("[5/5] Processing Gcode elapsed: {elapsed}"));
     gcode_bar.finish();
     if opt.debug {
         println!("gcode processing {:?}", clock.elapsed());
@@ -378,7 +324,7 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-pub fn partition_columns(data: &Vec<Point3d>) -> Vec<Vec<Point3d>> {
+pub fn partition_segments(data: &Vec<Point3d>, diameter: f32) -> Vec<Vec<Point3d>> {
     let mut output = Vec::new();
     let mut data = data.clone();
     while data.len() > 0 {
@@ -397,8 +343,38 @@ pub fn partition_columns(data: &Vec<Point3d>) -> Vec<Vec<Point3d>> {
                 })
                 .collect();
             column.sort();
-            output.push(column);
+            let mut segment = Vec::new();
+            let mut last = column[0];
+            for point in column {
+                if (point.pos.y - last.pos.y).abs() > diameter * 2. {
+                    output.push(segment.clone());
+                    segment.clear();
+                }
+                segment.push(point);
+                last = point;
+            }
+            output.push(segment);
         }
     }
     output
+}
+
+pub fn get_next_segment(segments: &mut Vec<Vec<Point3d>>, last: &Point3d) -> usize {
+    let mut dist = f32::MAX;
+    let mut current_segment = 0;
+    // TODO: see if splitting line segments gives better results
+    for (index, segment) in segments.iter_mut().enumerate() {
+        let curr_dist = distance(&segment[segment.len() - 1].pos.xy(), &last.pos.xy());
+        if curr_dist < dist {
+            dist = curr_dist;
+            current_segment = index;
+        }
+        let curr_dist = distance(&segment[0].pos.xy(), &last.pos.xy());
+        if curr_dist < dist {
+            dist = curr_dist;
+            segment.reverse();
+            current_segment = index;
+        }
+    }
+    current_segment
 }
