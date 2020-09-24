b6ca8842e521d6d784c1ae4750f93fbeedffeca8
Eric Trombly <etrombly@yahoo.com>
2020-09-24 15:15:07 -0500

    more work on path generation

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ use rayon::prelude::*;
 use std::{
     fs::File,
     io::{Read, Write},
-    sync::Arc,
+    sync::{Arc, Mutex},
     thread,
     time::Duration,
 };
 use structopt::StructOpt;
 
 pub fn generate_heightmap(
-    tests: Vec<Vec<Point3d>>,
+    tests: &[Vec<Point3d>],
     partition: Vec<Vec<Triangle3d>>,
     bar: &ProgressBar,
     total_bar: &ProgressBar,
@@ -41,25 +41,39 @@ pub fn generate_heightmap(
     result
 }
 
+pub fn generate_rest_map(heightmap: &[Vec<Point3d>]) -> Vec<Vec<Point3d>> {
+    heightmap
+            .iter()
+            .map(|column| {
+                column
+                    .iter()
+                    .map(|point| Point3d::new(point.pos.x, point.pos.y, 0.))
+                    .collect::<Vec<_>>()
+            })
+            .collect()
+}
+
 fn main() -> Result<()> {
     // parse input args, may remove this once I build the GUI
     let opt = Opt::from_args();
+    let radius = opt.diameter / 2.;
+    let stepover = opt.diameter * (opt.stepover / 100.);
+    let scale = 1. / opt.resolution;
+    let resolution = opt.resolution;
+    let tool = opt.tool.create(opt.diameter, opt.angle, scale);
 
     let mp = Arc::new(MultiProgress::with_draw_target(ProgressDrawTarget::stderr_nohz()));
     let partition_bar = mp.add(ProgressBar::new(0));
-    partition_bar.set_style(ProgressStyle::default_bar().template("[1/5] Filtering mesh"));
+    partition_bar.set_style(ProgressStyle::default_bar().template("[1/4] Filtering mesh"));
     partition_bar.tick();
     let height_bar = mp.add(ProgressBar::new(0));
-    height_bar.set_style(ProgressStyle::default_bar().template("[2/5] Computing height map"));
+    height_bar.set_style(ProgressStyle::default_bar().template("[2/4] Computing height map"));
     height_bar.tick();
     let tool_bar = mp.add(ProgressBar::new(0));
-    tool_bar.set_style(ProgressStyle::default_bar().template("[3/5] Processing tool path"));
+    tool_bar.set_style(ProgressStyle::default_bar().template("[3/4] Processing tool path"));
     tool_bar.tick();
-    let layer_bar = mp.add(ProgressBar::new(0));
-    layer_bar.set_style(ProgressStyle::default_bar().template("[4/5] Processing layers"));
-    layer_bar.tick();
     let gcode_bar = mp.add(ProgressBar::new(0));
-    gcode_bar.set_style(ProgressStyle::default_bar().template("[5/5] Processing Gcode"));
+    gcode_bar.set_style(ProgressStyle::default_bar().template("[4/4] Processing Gcode"));
     gcode_bar.tick();
     let total_bar = mp.add(ProgressBar::new(0));
     total_bar.set_style(ProgressStyle::default_bar().template("Total elapsed: {elapsed_precise}"));
@@ -71,19 +85,12 @@ fn main() -> Result<()> {
         thread::sleep(Duration::from_millis(10));
     });
 
-    let scale = 1. / opt.resolution;
-
     // open stl
     // TODO: give a nicer error if this isn't a valid stl
     let mut triangles = stl_to_tri(&opt.input)?;
 
     // initialize vulkan
     let vk = Vk::new()?;
-
-    // TODO: add support for multiple passes with different tools?
-    let radius = opt.diameter / 2.;
-    let stepover = opt.diameter * (opt.stepover / 100.);
-    let tool = opt.tool.create(radius, opt.angle, scale);
 
     if opt.debug {
         let mut file = File::create("tool.xyz")?;
@@ -101,16 +108,20 @@ fn main() -> Result<()> {
     // get bounds for the model
     let bounds = get_bounds(&triangles);
 
+    let stepdown = match opt.stepdown {
+        Some(x) => x,
+        None => bounds.p2.pos.z - bounds.p1.pos.z,
+    };
+
     // create the test points for the height map
     let grid = generate_grid(&bounds, &scale);
 
     // create columns
-    // TODO: add a spiral pattern
     let columns = generate_columns_chunks(&grid, &bounds, &opt.resolution, &scale);
 
     let clock = std::time::Instant::now();
     let partition = partition_tris(&triangles, &columns, &vk).unwrap();
-    partition_bar.set_style(ProgressStyle::default_bar().template("[1/5] Filtering mesh elapsed: {elapsed}"));
+    partition_bar.set_style(ProgressStyle::default_bar().template("[1/4] Filtering mesh elapsed: {elapsed}"));
     partition_bar.finish();
     total_bar.tick();
     if opt.debug {
@@ -129,80 +140,133 @@ fn main() -> Result<()> {
             let map: Vec<Vec<Point3d>> = bincode::deserialize(&buffer).unwrap();
             if map.len() != grid.len() && map[0].len() != grid[0].len() {
                 println!("Input heightmap does not match stl or resolution, recomputing");
-                generate_heightmap(grid, partition, &height_bar, &total_bar, &vk)
+                generate_heightmap(&grid, partition, &height_bar, &total_bar, &vk)
             } else {
                 map
             }
         },
-        _ => generate_heightmap(grid, partition, &height_bar, &total_bar, &vk),
+        _ => generate_heightmap(&grid, partition, &height_bar, &total_bar, &vk),
     };
 
-    height_bar.set_style(ProgressStyle::default_bar().template("[2/5] Computing height map elapsed: {elapsed}"));
+    height_bar.set_style(ProgressStyle::default_bar().template("[2/4] Computing height map elapsed: {elapsed}"));
     height_bar.finish();
     if opt.debug {
         println!("drop time {:?}", clock.elapsed());
     }
 
-    {
-        // write out height map
-        // TODO: add support for reading back in
-        let encoded = bincode::serialize(&heightmap).unwrap();
-        let mut file = File::create("out.map")?;
-        file.write_all(&encoded).unwrap();
-    }
-
     let clock = std::time::Instant::now();
     let segments = heightmap.len();
     let rows = heightmap[0].len();
-    // process height map with selected tool to find heights
-    let count = segments / (radius * stepover * scale).ceil() as usize;
-    tool_bar.set_length(count as u64);
-    tool_bar.reset_elapsed();
-    tool_bar.set_style(ProgressStyle::default_bar().template("[3/5] Processing tool path {bar:40.cyan/blue}"));
-    let processed: Vec<Vec<_>> = ((radius * scale) as usize..segments)
-        .into_par_iter()
-        // space each column based on radius and stepover
-        .step_by((radius * stepover * scale).ceil() as usize)
-        .map(|x| {
-            tool_bar.inc(1);
-            total_bar.tick();
-            // alternate direction for each column, have to collect into a vec to get types to match
-            /*
-            let steps = if column_num % 2 == 0 {
-                ((radius * scale) as usize..rows).collect::<Vec<_>>().into_par_iter()
+    // TODO: read in previous map if rest milling
+    let mut processed_map: Vec<Vec<_>> = match opt.restmap {
+        Some(file) => {
+            let mut file = File::open(file).unwrap();
+            let mut buffer = Vec::new();
+            file.read_to_end(&mut buffer).unwrap();
+            let map: Vec<Vec<Point3d>> = bincode::deserialize(&buffer).unwrap();
+            if map.len() != grid.len() && map[0].len() != grid[0].len() {
+                println!("Input heightmap does not match stl or resolution, recomputing");
+                generate_rest_map(&heightmap)
             } else {
-                ((radius * scale) as usize..rows).rev().collect::<Vec<_>>().into_par_iter()
-            };
-            */
-            let steps = (radius * scale) as usize..rows;
-            steps
-                .map(|y| {
-                    let max = tool
-                        .points
-                        .iter()
-                        .map(|tpoint| {
-                            // for each point in the tool adjust it's location to the height map and calculate the intersection
-                            let x_offset = (x as f32 + (tpoint.pos.x * scale)).round() as i32;
-                            let y_offset = (y as f32 + (tpoint.pos.y * scale)).round() as i32;
-                            if x_offset < segments as i32
-                                && x_offset >= 0
-                                && y_offset < rows as i32
-                                && y_offset >= 0
-                            {
-
-                                heightmap[x_offset as usize][y_offset as usize].pos.z
-                                    - tpoint.pos.z
-                            } else {
-                                bounds.p1.pos.z
-                            }
-                        })
-                        .fold(f32::NAN, f32::max); // same as calling max on all the values for this tool to find the heighest
-                    Point3d::new(x as f32 / scale, y as f32 / scale, max)
+                map
+            }
+        },
+        _ => generate_rest_map(&heightmap),
+    };
+    // TODO: rename to layers?
+    let mut layers = Vec::new();
+    // process height map with selected tool to find heights
+    tool_bar.reset_elapsed();
+    let mut count = 1;
+    tool_bar.set_style(ProgressStyle::default_bar().template("[3/4] Processing tool path {spinner} {msg}"));
+    loop {
+        tool_bar.set_message(&format!("pass {}", count));
+        tool_bar.tick();
+        total_bar.tick();
+        let mut current_layer_map = processed_map.clone();
+        let layer: Vec<Vec<Point3d>> = ((radius * scale) as usize..segments)
+        .into_par_iter()
+        // space each column based on diameter and stepover
+        .step_by((stepover * scale).ceil() as usize)
+        .map(|x| {
+            let mut column = Vec::new();
+            ((radius * scale) as usize..rows)
+            .for_each(|y| {
+                let mut max = tool
+                .points
+                .iter()
+                .map(|tpoint| {
+                    // for each point in the tool adjust it's location to the height map and calculate the intersection
+                    let x_offset = (x as f32 + (tpoint.pos.x * scale)).round() as i32;
+                    let y_offset = (y as f32 + (tpoint.pos.y * scale)).round() as i32;
+                    if x_offset < segments as i32
+                        && x_offset >= 0
+                        && y_offset < rows as i32
+                        && y_offset >= 0
+                    {
+                        heightmap[x_offset as usize][y_offset as usize].pos.z
+                        - current_layer_map[x_offset as usize][y_offset as usize].pos.z
+                        - tpoint.pos.z
+                    } else {
+                        f32::NAN
+                    }
                 })
-                .collect()
-        })
-        .collect();
-    tool_bar.set_style(ProgressStyle::default_bar().template("[3/5] Processing tool path elapsed: {elapsed}"));
+                .fold(f32::NAN, f32::max); // same as calling max on all the values for this tool to find the heighest
+                if max.abs() > stepdown {
+                    max = -stepdown;
+                }
+                if max.abs() < resolution {
+                    max = f32::NAN;
+                } 
+                
+                column.push(Point3d::new(x as f32 / scale, y as f32 / scale, max + current_layer_map[x][y].pos.z));
+            });
+            column
+        }).collect();
+        layer.iter().for_each(|column| {
+            column.iter().for_each(|point| {
+                tool.points.iter().for_each(|tpoint| {
+                    // for each point in the tool adjust it's location to the height map and
+                    // calculate the intersection
+                    let x_offset = ((point.pos.x + tpoint.pos.x) * scale).round() as i32;
+                    let y_offset = ((point.pos.y + tpoint.pos.y) * scale).round() as i32;
+                    if x_offset < segments as i32 && x_offset >= 0 && y_offset < rows as i32 && y_offset >= 0 {
+                        let new_pos = point.pos.z + tpoint.pos.z;
+                        if new_pos < current_layer_map[x_offset as usize][y_offset as usize].pos.z {
+                            current_layer_map[x_offset as usize][y_offset as usize].pos.z = new_pos;
+                        };
+                    }
+                });
+            })
+        });
+        layers.push(layer);
+        if current_layer_map.par_iter().enumerate().all(|(index, column)| {
+            column
+                .par_iter()
+                .enumerate()
+                .all(|(p_index, point)| approx_eq!(f32, point.pos.z, processed_map[index][p_index].pos.z, ulps = 2))
+        }) {
+            break;
+        }
+        {
+            let mut file = File::create(format!("layer{}.xyz", count))?;
+
+            let output = current_layer_map
+                .iter()
+                .flat_map(|column| {
+                    column
+                        .iter()
+                        .map(|point| format!("{:.3} {:.3} {:.3}\n", point.pos.x, point.pos.y, point.pos.z))
+                })
+                .collect::<Vec<String>>()
+                .join("");
+            file.write_all(output.as_bytes())?;
+        }
+        processed_map = current_layer_map.clone();
+        count += 1;
+    }
+
+    tool_bar.set_style(ProgressStyle::default_bar().template("[3/4] Processing tool path {msg} elapsed: {elapsed}"));
     tool_bar.finish();
     if opt.debug {
         println!("tool time {:?}", clock.elapsed());
@@ -223,42 +287,20 @@ fn main() -> Result<()> {
             .join("");
         file.write_all(output.as_bytes())?;
     }
+    {
+        // write out height map
+        // TODO: add support for reading back in
+        let encoded = bincode::serialize(&heightmap).unwrap();
+        let mut file = File::create("height.map")?;
+        file.write_all(&encoded).unwrap();
 
-    let clock = std::time::Instant::now();
-    // start multi-pass processing
-    let stepdown = match opt.stepdown {
-        Some(x) => x,
-        None => bounds.p2.pos.z - bounds.p1.pos.z,
-    };
-    let steps = ((bounds.p2.pos.z - bounds.p1.pos.z) / stepdown) as u64;
-    layer_bar.set_length(steps);
-    layer_bar.reset_elapsed();
-    layer_bar.set_style(ProgressStyle::default_bar().template("[4/5] Processing layers {bar:40.cyan/blue}"));
-    let points: Vec<Vec<Vec<_>>> = (1..steps + 1)
-        .map(|step| {
-            layer_bar.inc(1);
-            total_bar.tick();
-            processed
-                .iter()
-                .map(|row| {
-                    row.iter()
-                        .map(|x| match step as f32 * -stepdown {
-                            z if z > x.pos.z => Point3d::new(x.pos.x, x.pos.y, z),
-                            _ => *x,
-                        })
-                        .collect()
-                })
-                .collect()
-        })
-        .collect();
-    layer_bar.set_style(ProgressStyle::default_bar().template("[4/5] Processing layers elapsed: {elapsed}"));
-    layer_bar.finish();
-    if opt.debug {
-        println!("multi-pass processing {:?}", clock.elapsed());
+        let encoded = bincode::serialize(&processed_map).unwrap();
+        let mut file = File::create("rest.map")?;
+        file.write_all(&encoded).unwrap();
     }
 
     let clock = std::time::Instant::now();
-    gcode_bar.set_length(points.len() as u64);
+    gcode_bar.set_length(layers.len() as u64);
     gcode_bar.reset_elapsed();
     gcode_bar.set_style(ProgressStyle::default_bar().template("[5/5] Processing Gcode {bar:40.cyan/blue}"));
     let mut file = File::create(opt.output)?;
@@ -267,10 +309,10 @@ fn main() -> Result<()> {
     let mut output = format!("G0 Z{:.2} F300\n", 0.);
     let mut last = Point3d::new(0., 0., 0.);
 
-    for (layer_index, layer) in points.iter().enumerate() {
+    for layer in layers.iter() {
         gcode_bar.inc(1);
         total_bar.tick();
-        let mut islands = get_islands(&layer, bounds.p2.pos.z - (stepdown * (layer_index as f32)));
+        let mut islands = get_islands(&layer);
         let (mut current_island, _) = get_next_island(&islands, &last);
         // TODO: sort islands before processing
         while islands.len() > 0 {
@@ -295,14 +337,14 @@ fn main() -> Result<()> {
 
                 // don't write a move if only Y changed
                 while let Some(point) = segment.pop() {
-                    if !approx_eq!(f32, last.pos.x, point.pos.x, ulps = 3)
-                        || !approx_eq!(f32, last.pos.z, point.pos.z, ulps = 3)
-                    {
+                    //if !approx_eq!(f32, last.pos.x, point.pos.x, ulps = 3)
+                    //    || !approx_eq!(f32, last.pos.z, point.pos.z, ulps = 3)
+                    //{
                         output.push_str(&format!(
                             "G1 X{:.2} Y{:.2} Z{:.2}\n",
                             point.pos.x, point.pos.y, point.pos.z
                         ));
-                    }
+                    //}
 
                     last = point;
                 }
@@ -312,21 +354,21 @@ fn main() -> Result<()> {
                 ));
                 let tmp = get_next_segment(&mut segments, &last);
                 current_segment = tmp.0;
-                let dist = tmp.1;
+                //let dist = tmp.1;
                 // if we need to hop multiple columns see if there is a closer island
-                if dist > opt.diameter * 6. {
-                    let tmp = get_next_island(&islands, &last);
-                    if tmp.1 < dist {
-                        break;
-                    }
-                }
+                //if dist > opt.diameter * 6. {
+                //    let tmp = get_next_island(&islands, &last);
+                //    if tmp.1 < dist {
+                //        break;
+                //    }
+                //}
             }
             output.push_str(&format!("G0 Z{:.2}\n", 0.));
             let tmp = get_next_island(&islands, &last);
             current_island = tmp.0;
         }
     }
-    gcode_bar.set_style(ProgressStyle::default_bar().template("[5/5] Processing Gcode elapsed: {elapsed}"));
+    gcode_bar.set_style(ProgressStyle::default_bar().template("[4/4] Processing Gcode elapsed: {elapsed}"));
     gcode_bar.finish();
     if opt.debug {
         println!("gcode processing {:?}", clock.elapsed());
